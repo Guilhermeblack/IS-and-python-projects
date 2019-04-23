bddea82bb4c6786d64031754bf9a5e2ef67d729c
guilherme pereira <guilhermeglm1994@gmail.com>
2019-04-23 12:33:49 -0300

    Update cifra de cesar.py
    comentário do código para melhor interpretação de comandos

--- criptog/cifra de cesar.py
+++ criptog/cifra de cesar.py
@@ -2,6 +2,7 @@ print ("-=-=-=-=-=-=-=-=-=-=-=-=-=-")
 print("Criptografia de César")
 print(" ")
 
+#recebe a chave de critografia e valida
 chave = int(input("digite a chave de criptografia (ate 26)\n"))
 while chave > 26 or chave < 0:  
     print(" ")
@@ -9,36 +10,66 @@ while chave > 26 or chave < 0:
 
 base = 'abcdefghijklmnopqrstuvwxyz'
 
+#escolhe o modo
 modo = str(input("Deseja encriptar ou descriptar\n"))
 
+#insere e formata o texto
 text= input("Digite o texto a ser criptografado\n")
-texto = text(range(len(text)))
-#texto = texto.lower()
-#texto.split()
-txt = int(len(texto))
-num = 0
+tamanho_txt = len(text)
+text = text.lower()
+
+posicion = 0
+#insere o texto final
 cripto = ''
 
-#'''def crip(chave, base, modo, texto, txt):'''
-if (modo == 'e' or modo == 'encriptar') :
+#chama a função da criptografia
+def crip(chave, base, modo, texto, txt,cripto):
+    char = 0
+    #escolhe o modo
+    if (modo == 'e' or modo == 'encriptar') :
+    
+        while char <= tamaho_txt:
 
-    while txt>= 0:
-        for texto[txt] in base(range(len(base))):
-            num = base.find(texto[txt])
-            num= num + chave
-            if(num > len(base)):
-                    num = num- len(base)
-                    cripto = cripto + base[num]
-    txt= txt -1
+            #recebe o caractere de comparação
+            caractere = text[tamanho_txt]
+
+            #enquanto o caractere estiver coontido na base de comparação
+            for caractere in base:
+
+                #encontra o numero da posição dp caractere na base
+                posicion = base.find(caractere)
+
+                #soma a chave á posição
+                posicion += chave
+
+                # se a posição for maior que a base ira calcular a diferença
+                if(posicion > len(base)):
+                    posicion = posicion - len(base)
+
+        #concatena a o caractere encontrado           
+        cripto = cripto + base[posicion]
+
+        char +=1
                     
-if (modo == 'd' or modo == 'decriptar'):
-    while txt>= 0:
-        for texto[txt] in base[0:len(base)] :
-            num = base.find(texto[txt])
-            num = num - chave
-            if num < 0:
-                num = len(base)- abs(num)
-                cripto = cripto + base[num]
-    txt = txt -1      
-#    '''return cripto'''
+    if (modo == 'd' or modo == 'decriptar'):
+        # contador do texto
+        while char <= tamanho_txt:
+            # recebe o caractere da posição
+            caractere = text[tamanho_txt]
+            #condição se o caractere estiver contido na base
+            for caractere in base:
+                #encontra a posição
+                posicion = base.find(caractere)
+                #subtrai a chave
+                posicion -= chave
+                #condicional se a posição foi menor que 0
+                if posicion <= 0:
+                    # subtrai o valor absoluto da base para encontrar a posição
+                    posicion = len(base)- abs(posicion)
+                    
+        #resultado            
+        cripto = cripto + base[posicion]
+        char += 1      
+
+        return cripto
 print("sua mensagem \n" + cripto)
